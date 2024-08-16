4e4a095e6bda12666ac0afe4e6fb8fa5e14c7284
Katsu <paucastella2004@gmail.com>
2024-08-16 12:06:24 +0200

    Revert "bg change"
    This reverts commit 1276be8b692d4103ab24b0bf0042c94bb106094e.

--- portfolio/src/components/AboutMe.js
+++ portfolio/src/components/AboutMe.js
@@ -5,6 +5,7 @@ import './AboutMe.css';
 function AboutMe() {
     return (
         <section id="about">
+
             <p>
                 I’m Pau, "Katsu" is a nickname i got on all my accounts when i was little, and i've been keeping it since then.
             </p>
